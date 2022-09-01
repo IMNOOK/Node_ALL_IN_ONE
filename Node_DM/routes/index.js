78bcd07d579a9d30747da232d2ec4fa91ef5f7cf
IMNOOK <leeminwok0405@gmail.com>
2022-09-01 14:10:23 +0000

    what is problem to mongoose room?

--- Node_DM/routes/index.js
+++ Node_DM/routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const URL = 'https://node-all-in-one-api.run.goorm.io/v1';
 axios.defaults.headers.origin = 'https://node-all-in-one-dm.run.goorm.io';
 
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
+
 router.get('/', (req, res) => {
 	return res.render('main');
 })
@@ -79,21 +82,99 @@ router.get('/follow', async (req, res) => {
 
 router.post('/login', async (req, res) => {
 	try{
+		// 로그인 
 		const { email, password } = req.body;
 		let data = {id: 1, email, password};
 		const result = await request(req, '/login', data);
-		console.log(result);
 		if(!result.data.user){
 			return res.redirect('/');
 		}
 		req.session.user = result.data.user;
 		req.session.is_logined = true;
-		
-		data = {id: 2, userId: result.data.user.id};
+		const userId = result.data.user.id;
+		data = {id: 2, userId};
 		const result1 = await request(req, '/follow', data);
 		console.log(result1.data);
 		
-		return res.render('main', { user: result.data.user, });
+		//로그인 완료 이후 Room 가져오기
+		
+		const test = await Room.create({
+			aId: 0,
+			bId: 999,
+			title: 'sex',
+		});
+		
+		console.log(test);
+		
+		const testResult = await Room.find({
+			aId: 0,
+		});
+		console.log(testResult);
+		
+		let aId = []; //상대가 더 빨리 만든 아이디
+		let bId = []; //상대가 더 늦게 만든 아이디
+		result1.data.payload.forEach((obj) => {
+			if(obj.followerId > userId) {
+				bId.push(obj.followerId);	
+			} else{
+				aId.push(obj.followerId);
+			}
+		});
+		console.log(aId);
+		console.log(bId);
+		
+		let Room = [];
+		const aIdRooms = await Room.find({ //상대가 더 빨리 만든 아이디의 방
+			bId: userId
+		});
+		
+		const bIdRooms = await Room.find({ //상대가 더 늦게 만든 아이디의 방
+			aId: userId
+		});
+		
+		Room.push.apply(aIdRooms);
+		Room.push.apply(bIdRooms);
+		
+		Promise.all( aId.map(async (id) => {
+			let aIdCheck = (room) => {
+				if(room.aId === id){
+					return true;
+				}
+			}
+			
+			if(!aIdRooms.some(aIdCheck)){
+				console.log(id, '번 방이 없습니다.');
+				let newRoom = await Room.create({
+					aId: id,
+					bId: userId,
+					title: 'DM을 시작해보세요!',
+				});
+				console.log(newRoom, '방을 생성했습니다.');
+				Room.push(newRoom);
+			}
+		}))
+		
+		Promise.all( bId.map(async (id) => {
+			let bIdCheck = (room) => {
+				if(room.bId === id){
+					return true;
+				}
+			}
+			
+			if(!bIdRooms.some(bIdCheck)){
+				console.log(id, '번 방이 없습니다.');
+				let newRoom = await Room.create({
+					aId: userId,
+					bId: id,
+					title: 'DM을 시작해보세요!',
+				});
+				console.log(newRoom, '방을 생성했습니다.');
+				Room.push(newRoom);
+			}
+		}))
+		
+		console.log(Room);
+		return res.render('main', { user: result.data.user, rooms: Room });
 	} catch(err) {
 		console.error(err);
 		next(err);
